1c1d4403ec8f354941b9562e7a91e4621e176acf
pras75299 <singh.prashantking@gmail.com>
2024-10-19 18:26:23 +0530

    add css to cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useContext } from "react";
 import "./App.css";
 import Item from "./component/Item";
 import Cart from "./component/Cart";
 function App() {
   return (
     <>
-      <Item name="Macbook" price={1000} />
-      <Item name="Disk" price={1200} />
-      <Item name="Pendrive" price={100} />
-      <Item name="Case" price={200} />
+      <div className="items-container">
+        <Item name="Macbook" price={1000} />
+        <Item name="Disk" price={1200} />
+        <Item name="Pendrive" price={100} />
+        <Item name="Case" price={200} />
+        <Item name="Classbook" price={100} />
+        <Item name="Pen" price={200} />
+      </div>
       <Cart />
     </>
   );
